26ecb74e10e053bd83e9f614207388bf9e4c9f23
Biscolata00 <leyla.otzgh@gmail.com>
2026-07-07 16:54:06 +0200

    compressed backgrounds

--- navbar_and_basics.js
+++ navbar_and_basics.js
@@ -127,12 +127,12 @@ const footerEl = `
         <div class="footer">
         <footer>
         <br>
-            <img src="" class="img_footer">
+            
         </footer>
         </div>
 `;
 
-//${nesting}/images/watermelon.gif
+//<img src="${nesting}/images/watermelon.gif" class="img_footer">
 
 // Insert your sidebar HTML inside these ``. You can use HTML as usual.
 // Remove all the content inside the `` if you don't have a sidebar.
